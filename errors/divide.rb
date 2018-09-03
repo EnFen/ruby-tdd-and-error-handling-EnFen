231fb54cf59584c7b48c69d81d55798a21612d49
Nathan <nathan.fenimore@gmail.com>
2018-09-03 15:24:23 +1000

    initial commit

--- errors/divide.rb
+++ errors/divide.rb
@@ -4,30 +4,54 @@
 # Let's bring in some error handling to display a nice message.
 #
 
-class String
-    def is_i?
-       /\A[-+]?\d+\z/ === self
-    end
-end
-
-def divide (dividend,divisor)
-    quotient = dividend/divisor
-end
-
-puts "--------------------"
-puts "--- Division App ---"
-puts "--------------------"
-begin
-    puts "Give me a number"
-    number1 = gets.chomp.to_i
-    puts "Give me another number"
-    number2 = gets.chomp.to_i
-    answer = divide(number1,number2)
-    print "#{number1} divided by #{number2} = "
-    print "#{answer || "error"}"
-    puts
-rescue
-    puts "Error occured"
-end
+#
 #
 # Code along challenge: if they hit an error, take them back to "give me a number"
+
+# Refactoring classmates code
+
+# ask user for 3+ grocery items and store in a list
+# .length how many items in the list
+# .sort the list alphabetically
+# three times loop using count command
+# "What quantity would you like?" !!!
+# puts "What quantity of #{item} do you want?"
+
+grocery_list = []
+quantities = []
+count = 0
+puts "What grocery items do you need?"
+item = gets.chomp
+    until count == 3
+        #add grocery item to list
+        grocery_list.push(item)
+        puts "What quantity would you like?" # interpolate item?
+        quantity = gets.chomp #specifiy as integer?
+        quantities.push(quantity)
+        if count == 2
+            puts "Thanks for shopping!"
+        else
+            puts "What else do you need?"
+            item = gets.chomp
+        end
+        count += 1
+    end
+
+# replace ice-cream with broccoli
+grocery_list = grocery_list.map {|item|item == "ice-cream"? "broccoli":item}
+
+# Testing commands
+puts "You have #{grocery_list.length} items in your list."
+#puts "Grocery List: #{grocery_list.zip(quantities).sort.join(", ")}"
+
+# QUANTITY IDEAS
+# 1. create new array for quantities & combine when printing list
+#   printing ideas:
+#   - access and link elements from both arrays
+#   - align elements from arrays using interpolation !doesn't sort
+# 2. use hash to assign a quanity value to items key
+# 3. push quanities into item array and access/print in pairs
+# issues: quantities are included in .length so prints as 6 items
+
+puts "GROCERY LIST"
+puts grocery_list.zip(quantities).join(", ")
